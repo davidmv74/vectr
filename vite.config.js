f8c88584ccd00cb14145bc54af67a14ab4632197
David Pacheco <dpacheco@Davids-MacBook-Pro.local>
2025-07-04 16:48:57 -0400

    Adding deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import ghPages from 'vite-plugin-gh-pages';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react(), ghPages()],
-  base: '/vectr-studio-site/', // this is required
+  base: '/vectr/',
+  plugins: [react()],
 });
